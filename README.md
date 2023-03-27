30eb90b45d16ba1fd68dceade064ec01f7759a4f
1d8 <bnu1s@protonmail.com>
2023-03-26 19:18:53 -0700

    Added scripts, readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# elk
+# ELK Stack Installation Script
+
+* `elk-stack.sh`
+
+When running `elk-stack.sh`, you have to provide an argument that corresponds to your installation type. The choices include:
+
+* *Server* - installs the entire ELK stack onto a machine. Uses Docker-compose to do so.
+* *Client* - installs the logbeat client. For Linux boxes only. If you choose this option, you must also provide the IP address of the ELK stack server. Example usage:
+	* `./elk-stack.sh client 192.168.1.5`
+
+The script must be ran as root. 
+
+* `winelk.ps1`
+
+When running `winelk.ps1`, you have to provide an argument that corresponds to the IP address of the ELK stack server.
+
+* Example usage: `./winelk.ps1 192.168.1.5`
+* The script will install winlogbeat & modify the configuration file to match the IP address of the ELK server that you provide
+
+The script must be ran with administrative privileges.
---
+++ winelk.ps1
@@ -0,0 +1,25 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$ELKIPAddress
+)
+
+# Download the Winlogbeat installation package
+Invoke-WebRequest -Uri "https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.14.1-windows-x86_64.zip" -OutFile "$env:TEMP\winlogbeat.zip"
+
+# Extract the Winlogbeat installation package
+Expand-Archive -Path "$env:TEMP\winlogbeat.zip" -DestinationPath "$env:ProgramFiles\Winlogbeat" -Force
+
+# Configure Winlogbeat
+$winlogbeatConfigPath = "$env:ProgramFiles\Winlogbeat\winlogbeat-7.14.1-windows-x86_64\winlogbeat.yml"
+$winlogbeatConfig = Get-Content $winlogbeatConfigPath
+$winlogbeatConfig = $winlogbeatConfig.replace("#output.elasticsearch:", "output.elasticsearch:")
+#$winlogbeatConfig = $winlogbeatConfig -replace "#hosts: \[\"localhost:9200\"\]","hosts: [\"$($ELKIPAddress):9200\"]"
+$winlogbeatConfig = $winlogbeatConfig -replace 'hosts: \["localhost:9200"\]', ('hosts: ["{0}:9200"]' -f $ELKIPAddress)
+#$winlogbeatConfig = $winlogbeatConfig -replace 'hosts: \["localhost:9200"\]', "hosts: [""$ELKIPAddress:9200""]"
+Set-Content $winlogbeatConfigPath $winlogbeatConfig
+
+# Install Winlogbeat as a service
+& "$env:ProgramFiles\Winlogbeat\winlogbeat-7.14.1-windows-x86_64\install-service-winlogbeat.ps1"
+
+# Start the Winlogbeat service
+Start-Service winlogbeat
